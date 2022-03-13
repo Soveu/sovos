414a99ee4cda97f83866cd5d7ec46b2ed785050c
Soveu <marx.tomasz@gmail.com>
2022-03-13 12:24:12 +0100

    remove() is not working, probably need some rewrite at this point

--- libs/freetree/src/poc.rs
+++ libs/freetree/src/poc.rs
@@ -35,6 +35,13 @@ impl Root {
     pub fn search(&self, p: usize) -> bool {
         self.0.search(p)
     }
+
+    pub fn remove(&mut self, p: usize) -> Option<Edge> {
+        match self.0.find_and_remove(p) {
+            RemovalResult::NotFound => None,
+            RemovalResult::Done(e) | RemovalResult::Underflow(e) => Some(e),
+        }
+    }
 }
 
 pub type Edge = Unique<[Node; 2]>;
@@ -152,25 +159,218 @@ impl Node {
         return InsertionResult::Overflow(median);
     }
 
-    fn remove_leaf(&mut self, index: usize) -> RemovalResult {
-        let item = self.edges.remove(index);
 
+
+
+
+
+
+
+    fn try_remove(&mut self, index: usize) -> RemovalResult {
+        let item = self.edges.remove(index);
         if self.edges.len() < B {
             return RemovalResult::Underflow(item);
         }
         return RemovalResult::Done(item);
     }
 
-    fn remove(&mut self, index: usize) -> RemovalResult {
-        let leaf = self.grab_next_leaf();
-        let item = mem::replace(&mut self.edges[index], leaf);
-        todo!("found it, but it might be an intermediate node");
-    }
-    fn grab_next_leaf(&mut self) -> Edge {
-        if self.edges[0][LEFT_NODE_IDX].edges.len() != 0 {
-            return self.edges[0][LEFT_NODE_IDX].grab_next_leaf();
+    fn grab_leaf(&mut self) -> RemovalResult {
+        if self.edges.is_empty() {
+            return RemovalResult::NotFound;
         }
-        todo!("return self.remove_leaf(i) and rebalance");
+        if self.edges[0][LEFT_NODE_IDX].edges.is_empty() {
+            return self.try_remove(0);
+        }
+        return match self.edges[0][LEFT_NODE_IDX].grab_leaf() {
+            RemovalResult::Underflow(leaf) => self.rebalance(0, leaf),
+            x => x,
+        };
+    }
+
+    fn replace(dst: &mut Edge, mut src: Edge) -> RemovalResult {
+        debug_assert!(src[RIGHT_NODE_IDX].edges.is_empty());
+
+        src[RIGHT_NODE_IDX].edges.extend(dst[RIGHT_NODE_IDX].edges.drain(..));
+        src[LEFT_NODE_IDX].edges.extend(dst[LEFT_NODE_IDX].edges.drain(..));
+        return RemovalResult::Done(mem::replace(dst, src));
+    }
+
+    fn remove(&mut self, index: usize) -> RemovalResult {
+        return match self.edges[index][RIGHT_NODE_IDX].grab_leaf() {
+            RemovalResult::NotFound => self.try_remove(index),
+            RemovalResult::Done(leaf) => Self::replace(&mut self.edges[index], leaf),
+            RemovalResult::Underflow(leaf) => {
+                let new = &mut self.edges[index];
+                let mut ret = mem::replace(new, leaf);
+                new[LEFT_NODE_IDX].edges.extend(ret[LEFT_NODE_IDX].edges.drain(..));
+                new[RIGHT_NODE_IDX].edges.extend(ret[RIGHT_NODE_IDX].edges.drain(..));
+                self.rebalance(index+1, ret)
+            },
+        };
+    }
+
+    fn rotate_right(left: &mut Self, parent: &mut Edge) {
+        let new_parent = left.edges.pop().unwrap();
+        let mut old_parent = mem::replace(parent, new_parent);
+
+        old_parent[LEFT_NODE_IDX].edges.extend(parent[RIGHT_NODE_IDX].edges.drain(..));
+        parent[RIGHT_NODE_IDX].edges.push(old_parent);
+
+        unsafe {
+            let old_parent: *mut Self = &mut parent[RIGHT_NODE_IDX].edges[0][RIGHT_NODE_IDX];
+            let old_parent = &mut *old_parent;
+            parent[RIGHT_NODE_IDX].edges.extend(old_parent.edges.drain(..));
+        }
+
+        let (old_parent, rest) = parent[RIGHT_NODE_IDX].edges.split_first_mut().unwrap();
+        old_parent[RIGHT_NODE_IDX].edges.extend(rest[0][LEFT_NODE_IDX].edges.drain(..));
+    }
+
+    fn rotate_left(left: &mut Self, parent: &mut Edge) {
+        let (new_parent, rest) = parent[RIGHT_NODE_IDX].edges.split_first_mut().unwrap();
+        rest[0][LEFT_NODE_IDX].edges.extend(new_parent[RIGHT_NODE_IDX].edges.drain(..));
+
+        unsafe {
+            let new_parent: *mut Self = &mut parent[RIGHT_NODE_IDX].edges[0][RIGHT_NODE_IDX];
+            let new_parent = &mut *new_parent;
+            new_parent.edges.extend(parent[RIGHT_NODE_IDX].edges.drain(1..));
+        }
+
+        let new_parent = parent[RIGHT_NODE_IDX].edges.pop().unwrap();
+        let mut old_parent = mem::replace(parent, new_parent);
+        old_parent[RIGHT_NODE_IDX].edges.extend(parent[LEFT_NODE_IDX].edges.drain(..));
+        left.edges.push(old_parent);
+    }
+
+    fn check_size_and_ret(&self, to_return: Edge) -> RemovalResult {
+        if self.edges.len() < B {
+            return RemovalResult::Underflow(to_return);
+        }
+        return RemovalResult::Done(to_return);
+    }
+
+    fn merge(&mut self, index: usize) {
+        debug_assert!(index >= 2);
+        let left = index - 2;
+        let right = index - 1;
+        let right = self.edges.remove(right);
+        let left = &mut self.edges[left][RIGHT_NODE_IDX].edges;
+        debug_assert!(left.len() == B);
+
+        left.push(right);
+        let last: *mut Self = &mut left.last_mut().unwrap()[RIGHT_NODE_IDX];
+        let next = left.len();
+        unsafe {
+            let last = &mut *last;
+            left.extend(last.edges.drain(..));
+            last.edges.extend(left[next][LEFT_NODE_IDX].edges.drain(..));
+        }
+    }
+
+    fn merge_first(&mut self, to_return: Edge) -> RemovalResult {
+        let mut e = self.edges.remove(0);
+
+        //root case
+        if self.edges.is_empty() {
+            self.edges.extend(e[LEFT_NODE_IDX].edges.drain(..));
+            self.edges.push(e);
+            let last: *mut Self = &mut self.edges.last_mut().unwrap()[RIGHT_NODE_IDX];
+            let next = self.edges.len();
+            unsafe {
+                let last = &mut *last;
+                self.edges.extend(last.edges.drain(..));
+                last.edges.extend(self.edges[next][LEFT_NODE_IDX].edges.drain(..));
+            }
+            return RemovalResult::Done(to_return);
+        }
+
+        let new = &mut self.edges[0][LEFT_NODE_IDX].edges;
+        new.extend(e[LEFT_NODE_IDX].edges.drain(..));
+        new.push(e);
+        let last: *mut Self = &mut new.last_mut().unwrap()[RIGHT_NODE_IDX];
+        let next = new.len();
+
+        /* SAFETY: we are not aliasing and ArrayVec doesn't invalidate pointers after extend() */
+        unsafe {
+            let last = &mut *last;
+            new.extend(last.edges.drain(..));
+            let next = &mut new[next][LEFT_NODE_IDX];
+            //debug_assert!(next.edges.len() >= B);
+            last.edges.extend(next.edges.drain(..))
+        }
+
+        return self.check_size_and_ret(to_return);
+    }
+
+    fn rebalance(&mut self, index: usize, to_return: Edge) -> RemovalResult {
+        debug_assert!(index <= TWO_B);
+        //debug_assert!(self.edges.len() >= B, "This method can't be called on root!");
+
+        if index == 0 || (/* root case */index == 1 && self.edges.len() == 1) {
+            if self.edges[0][RIGHT_NODE_IDX].edges.len() <= B {
+                return self.merge_first(to_return);
+            }
+            /* Rotate left */
+            let right = &mut self.edges[0][RIGHT_NODE_IDX];
+            let mut i = right.edges.iter_mut();
+            let right_left: *mut Edge = i.next().unwrap();
+            let right_new_left = i.next().unwrap();
+
+            // SAFETY: we are not aliasing, because 1..
+            unsafe {
+                let right_left = &mut *right_left;
+                right_new_left[LEFT_NODE_IDX].edges.extend(right_left[RIGHT_NODE_IDX].edges.drain(..));
+                right_left[RIGHT_NODE_IDX].edges.extend(right.edges.drain(1..));
+            }
+
+            let new_parent = right.edges.pop().unwrap();
+            let parent = &mut self.edges[0];
+            let mut old_parent = mem::replace(parent, new_parent);
+            debug_assert_eq!(old_parent[RIGHT_NODE_IDX].edges.len(), 0);
+            old_parent[RIGHT_NODE_IDX].edges.extend(parent[LEFT_NODE_IDX].edges.drain(..));
+            parent[LEFT_NODE_IDX].edges.extend(old_parent[LEFT_NODE_IDX].edges.drain(..));
+            parent[LEFT_NODE_IDX].edges.push(old_parent);
+
+            return self.check_size_and_ret(to_return);
+        }
+
+        if /*root casen't*/self.edges.len() > 1 && index == self.edges.len() {
+            if self.edges[index-2][RIGHT_NODE_IDX].edges.len() <= B {
+                self.merge(index);
+                return self.check_size_and_ret(to_return);
+            }
+            let (parent, left) = self.edges.split_last_mut().unwrap();
+            let left = left.last_mut().unwrap();
+            Self::rotate_right(&mut left[RIGHT_NODE_IDX], parent);
+            return self.check_size_and_ret(to_return);
+        }
+
+        let parent: *mut Edge = &mut self.edges[index.saturating_sub(1)];
+        let left = if index == 1 {
+            &mut self.edges[0][LEFT_NODE_IDX]
+        } else {
+            &mut self.edges[index-1][RIGHT_NODE_IDX]
+        };
+        if left.edges.len() > B {
+            Self::rotate_right(left, unsafe { &mut *parent });
+            return RemovalResult::Done(to_return);
+        }
+
+        //todo!("this is shit");
+        let right_parent = &mut self.edges[index];
+        if right_parent[RIGHT_NODE_IDX].edges.len() > B {
+            let parent = unsafe { &mut *parent };
+            Self::rotate_left(&mut parent[RIGHT_NODE_IDX], right_parent);
+            return RemovalResult::Done(to_return);
+        }
+
+        // index == 0 is covered at the beginning
+        if index == 1 {
+            return self.merge_first(to_return);
+        }
+
+        self.merge(index);
+        return self.check_size_and_ret(to_return);
     }
 
     pub fn find_and_remove(&mut self, p: usize) -> RemovalResult {
@@ -186,21 +386,40 @@ impl Node {
 
         let node = match index {
             0 => &mut self.edges[0][LEFT_NODE_IDX],
-            i => &mut self.edges[i][RIGHT_NODE_IDX],
+            i => &mut self.edges[i-1][RIGHT_NODE_IDX],
         };
 
-        return node.find_and_remove(p);
+        return match node.find_and_remove(p) {
+            RemovalResult::Underflow(e) => self.rebalance(index, e),
+            x => x,
+        };
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 impl fmt::Debug for Node {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let self_addr = self as *const Self as usize;
+        let _ = write!(f, "\n{:x}: ", self_addr)?;
         if self.edges.len() == 0 {
             return Ok(());
         }
 
-        let self_addr = self as *const Self as usize;
-        let _ = write!(f, "\n{:x}: ", self_addr)?;
         let iter = self.edges
             .iter()
             .map(|p| &p[0] as *const Node);
--- libs/freetree/tests/test.rs
+++ libs/freetree/tests/test.rs
@@ -24,7 +24,7 @@ fn xorshift(mut x: u32) -> u32 {
 fn test1() {
     let mut root = ManuallyDrop::new(Root::new());
     let mut seed = 0xDEADBEEF;
-    let mut allocations: Vec<_> = (0..5_000_000)
+    let mut allocations: Vec<_> = (0..100_000)
         .into_iter()
         .map(|_| Unique::into_raw(new_edge()) as usize)
         .collect();
@@ -60,4 +60,45 @@ fn test1() {
     println!(" {:?}", now.elapsed());
 
     assert!(root.search(0) == false);
+}
+
+#[test]
+fn test2() {
+    let mut root = ManuallyDrop::new(Root::new());
+    let mut seed = 0xDEADBEEF;
+    let mut allocations: Vec<_> = (0..36)
+        .into_iter()
+        .map(|_| Unique::into_raw(new_edge()) as usize)
+        .collect();
+
+    for i in 0..allocations.len() {
+        seed = xorshift(seed);
+        let index = seed as usize;
+        let index = index % (allocations.len() - i);
+        let index = i + index;
+        allocations.swap(i, index);
+    }
+
+    print!("Inserting elements");
+    let now = Instant::now();
+    for edge in allocations.iter().copied() {
+        let edge = unsafe { Unique::from_raw(edge as *mut _) };
+        //println!("\nInserting {:x}", p);
+        //println!("{:?}", root);
+        root.insert(edge);
+        //assert!(root.search(p), "where is {:X}?\n{:?}", p, root);
+    }
+    println!(" {:?}", now.elapsed());
+
+    //println!("{:?}", root);
+
+    let now = Instant::now();
+    for p in allocations {
+        println!("Removing {:X}", p);
+        println!("{:?}", root);
+        let res = ManuallyDrop::new(root.remove(p));
+        assert_eq!(res.as_ref().unwrap().as_usize(), p);
+    }
+    println!(" {:?}", now.elapsed());
+
 }
